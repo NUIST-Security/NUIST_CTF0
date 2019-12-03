449f23dd8fadf7c094623c3fcf500cbf08025acf
Horzioncy <1457815071@qq.com>
2019-12-03 14:28:20 +0800

    pwn

--- problems/pwn/pwn.c
+++ problems/pwn/pwn.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 char buf[32];
 int main(int argc, char* argv[], char* envp[]){
-        if(argc<2){
-                printf("pass argv[1] a number\n");
-                return 0;
-        }
-        int fd = atoi( argv[1] ) - 0x1234;
-        int len = 0;
-        len = read(fd, buf, 32);
+		int in, len, fd;
+		char *str = "12345.67";
+		scanf("%d", &in);
+		fd = in - atoi(str);
+        read(fd, buf, 32);
         if(!strcmp("LETMEWIN\n", buf)){
                 printf("good job :)\n");
                 system("/bin/cat flag");
