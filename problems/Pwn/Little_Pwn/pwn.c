8fd7299af7c0b542166c875232b578733f4f958f
Horzioncy <1457815071@qq.com>
2019-12-10 19:47:02 +0800

    pwn

---
+++ problems/Pwn/Little_Pwn/pwn.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+char buf[32];
+int main(int argc, char* argv[], char* envp[]){
+		int in, len, fd;
+		char *str = "12345.67";
+		scanf("%d", &in);
+		fd = in - atoi(str);
+        read(fd, buf, 32);
+        if(!strcmp("LETMEWIN\n", buf)){
+                printf("good job :)\n");
+                system("/bin/sh");
+                exit(0);
+        }
+        printf("learn about Linux file IO\n");
+        return 0;
+
+}
