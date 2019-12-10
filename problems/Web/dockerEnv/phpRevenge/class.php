500d876dbe926e1438a3e4ab716fc3c905c4f83f
needhourger <841904608@qq.com>
2019-12-10 14:02:28 +0800

    add dockerEnv

---
+++ problems/Web/dockerEnv/phpRevenge/class.php
@@ -0,0 +1,14 @@
+
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+ 
+class Read{//f1a9.php
+    public $file;
+    public function __toString(){
+        if(isset($this->file)){
+            echo file_get_contents($this->file);    
+        }
+        return "__toString was called!";
+    }
+}
+?>
