a571f5efcabe5b2c01cf5ad0b0c1cca9b04b17fc
needhourger <841904608@qq.com>
2019-12-13 10:47:59 +0800

    fix bugs

--- problems/run.sh
+++ problems/run.sh
@@ -1,21 +1,11 @@
 # web
-docker run  -p 1274:80 -p 1275:81 -p 1276:82 \ 
-            -v /root/NUIST_CTF0/problems/Web/docker1/conf.d:/etc/nginx/conf.d \
-            -v /root/NUIST_CTF0/problems/Web/docker1/www:/var/www \
-            --rm --name=nginx -d nginx:latest
+docker run  -p 1274:80 -p 1275:81 -p 1276:82 -v /root/NUIST_CTF0/problems/Web/docker1/conf.d:/etc/nginx/conf.d -v /root/NUIST_CTF0/problems/Web/docker1/www:/var/www --rm --name=nginx1 -d nginx:latest
 
-docker run  -p 9000:9000 \
-            -v /root/NUIST_CTF0/problems/Web/docker1/www:/var/www \
-            --rm --name=php -d php:7.4-fpm
+docker run  -p 9000:9000 -v /root/NUIST_CTF0/problems/Web/docker1/www:/var/www --rm --name=php1 -d php:7.4-fpm
 
-docker run  -p 1277:80 \ 
-            -v /root/NUIST_CTF0/problems/Web/docker2/conf.d:/etc/nginx/conf.d \
-            -v /root/NUIST_CTF0/problems/Web/docker2/www:/var/www \
-            --rm --name=nginx -d nginx:latest
+docker run  -p 1277:80 -v /root/NUIST_CTF0/problems/Web/docker2/conf.d:/etc/nginx/conf.d -v /root/NUIST_CTF0/problems/Web/docker2/www:/var/www --rm --name=nginx2 -d nginx:latest
 
-docker run  -p 9001:9000 \
-            -v /root/NUIST_CTF0/problems/Web/dcoker2/www:/var/www \
-            --rm --name=php -d php:7.4-fpm
+docker run  -p 9001:9000 -v /root/NUIST_CTF0/problems/Web/docker2/www:/var/www --rm --name=php2 -d php:7.4-fpm
 
 # pwn build
 docker build -t littlepwn .
